b737b2c98ceb124f0fdd113323d339c844ee45b3
Will Price <will.price94@gmail.com>
2014-03-21 17:34:37 +0000

    Use `bind` to handle `this` in modularised create_db code

--- src/create_db.js
+++ src/create_db.js
@@ -1,104 +1,115 @@
-"use_strict";
+"use strict";
 
-function dropPreviouslyCreatedTables(table_list)
-{
-    var num_tables = table_list.length;
-    var tables_dropped = 0;
+var contents = {
+    that: this,
 
-    for(var i = 0; i < num_tables; i++)
+    dropPreviouslyCreatedTables: function (table_list)
     {
-        db.run("DROP TABLE IF EXISTS " + table_list[i], function(err,data) {
-            if(err) { throw err; }
-
+        var num_tables = table_list.length;
+        var tables_dropped = 0;
+        var that = this;
+        function logTableDropIncrementCount(err) {
+            if (err) { throw err; }
             console.log("Dropped table");
-
             tables_dropped++;
+        }
 
-            if(tables_dropped === num_tables)
-            {
+        function ifAllTablesDroppedCreateFilesTable() {
+            if (tables_dropped === num_tables) {
                 console.log("Finished dropping tables");
-                createFilesTable();
+                that.createFilesTable();
             }
-        });
+        }
+
+        function dropTablesCallback(err, data) {
+            logTableDropIncrementCount(err);
+            ifAllTablesDroppedCreateFilesTable.call(that);
+        }
+
+        for (var i = 0; i < num_tables; i++) {
+            db.run("DROP TABLE IF EXISTS " + table_list[i], dropTablesCallback);
+        }
+    },
+
+  createFilesTable: function ()
+    {
+        var sql_stmt = "CREATE TABLE files (" +
+            "files_id INTEGER PRIMARY KEY, " +
+            "name TEXT, " +
+            "graph TEXT, " +
+            "pos TEXT, " +
+            "map BLOB )";
+
+        function logAndCreateSessionTable(err) {
+            if (err) { throw err; }
+            console.log("Table 'files' created");
+            this.createSessionTable();
+        }
+
+        db.run(sql_stmt, logAndCreateSessionTable.bind(this));
+    },
+
+    createSessionTable: function ()
+    {
+        var sql_stmt = "CREATE TABLE session (" +
+            "session_id INTEGER PRIMARY KEY," +
+            "name TEXT, " +
+            "files_id INTEGER, " +
+            "FOREIGN KEY(files_id) REFERENCES files(files_id))";
+
+        function logAndCreatePlayerTable(err) {
+            if (err) { throw err; }
+            console.log("Table 'session' created");
+            this.createPlayerTable();
+        }
+
+        db.run(sql_stmt, logAndCreatePlayerTable.bind(this));
+    },
+
+    createPlayerTable: function ()
+    {
+        var sql_statement = "CREATE TABLE player (" +
+            "player_id INTEGER PRIMARY KEY, " +
+            "type TEXT, " +
+            "location INTEGER, " +
+            "taxi_tickets INTEGER, " +
+            "bus_tickets INTEGER, " +
+            "underground_tickets INTEGER, " +
+            "double_tickets INTEGER, " +
+            "secret_tickets INTEGER, " +
+            "session_id INTEGER, " +
+            "FOREIGN KEY(session_id) REFERENCES session(session_id))";
+
+        function logAndCreateMoveTable(err) {
+            if (err) { throw err; }
+            console.log("Table 'player' created");
+            this.createMoveTable();
+        }
+
+        db.run(sql_statement, logAndCreateMoveTable.bind(this));
+    },
+
+    createMoveTable: function () {
+        var sql_statement = "CREATE TABLE move (" +
+            "move_id INTEGER PRIMARY KEY, " +
+            "start_location INTEGER, " + "end_location INTEGER, " +
+            "ticket_type TEXT, " +
+            "player_id INTEGER, " +
+            "FOREIGN KEY(player_id) REFERENCES player(player_id))";
+
+        function log(err) {
+            if (err) { throw err; }
+            console.log("Table 'move' created");
+        }
+
+        db.run(sql_statement, log);
+
     }
-}
+};
 
-function createFilesTable()
-{
-    var sql_stmt = "CREATE TABLE files (" +
-        "files_id INTEGER PRIMARY KEY, " +
-        "name TEXT, " +
-        "graph TEXT, " +
-        "pos TEXT, " +
-        "map BLOB )";
-
-    db.run(sql_stmt, function(err) {
-        if(err) { throw err; }
-
-        console.log("Table 'files' created");
-        createSessionTable();
-    });
-}
-
-function createSessionTable()
-{
-    var sql_stmt = "CREATE TABLE session (" +
-        "session_id INTEGER PRIMARY KEY," +
-        "name TEXT, " +
-        "files_id INTEGER, " +
-        "FOREIGN KEY(files_id) REFERENCES files(files_id))";
-
-    db.run(sql_stmt, function(err) {
-        if(err) { throw err; }
-
-        console.log("Table 'session' created");
-
-        createPlayerTable();
-    });
-}
-
-function createPlayerTable()
-{
-    var sql_statement = "CREATE TABLE player (" +
-        "player_id INTEGER PRIMARY KEY, " +
-        "type TEXT, " +
-        "location INTEGER, " +
-        "taxi_tickets INTEGER, " +
-        "bus_tickets INTEGER, " +
-        "underground_tickets INTEGER, " +
-        "double_tickets INTEGER, " +
-        "secret_tickets INTEGER, " +
-        "session_id INTEGER, " +
-        "FOREIGN KEY(session_id) REFERENCES session(session_id))";
-
-    db.run(sql_statement, function(err) {
-        if (err) { throw err; }
-
-        console.log("Table 'player' created");
-
-        createMoveTable();
-    });
-}
-
-function createMoveTable() {
-    var sql_statement = "CREATE TABLE move (" +
-        "move_id INTEGER PRIMARY KEY, " +
-        "start_location INTEGER, " +
-        "end_location INTEGER, " +
-        "ticket_type TEXT, " +
-        "player_id INTEGER, " +
-        "FOREIGN KEY(player_id) REFERENCES player(player_id))";
-
-    db.run(sql_statement, function(err) {
-        if (err) { throw err; }
-
-        console.log("Table 'move' created");
-    });
-
-}
+module.exports = contents;
 
 var sql = require("sqlite3");
 var db = new sql.Database("test.db");
 var table_list = ['files', 'session', 'player', 'move'];
-dropPreviouslyCreatedTables(table_list);
-
+contents.dropPreviouslyCreatedTables(table_list);
--- src/db_access_test.js
+++ src/db_access_test.js
@@ -1,30 +1,28 @@
 "use_strict";
 
-// here we define the callback function that is going to be passed into
-// the getFile db_access function. This function will be triggered 
-// when the getFile function has performed its database query
+var sql = require('sqlite3');
+var test = require('unit.js');
+
+var db_access = require('./db_access');
+var create_db = require('./create_db');
+
+function createTestTable()
+{
+    var db = new sql.Database(':memory:');
+}
+
 function dbCallback(err, data)
 {
-	// first we check if the db function has produced an error
-	// if it has we throw it so that we can see the error
-	if(err) throw err;
+	if(err) { throw err; }
 
-	// if we get here we know the function was successful so 
-	// we can now acces the data
 	console.log("Extracted File has " + data.length + " characters");
-	data_lines = data.split("\n");
+	var data_lines = data.split("\n");
 	console.log("Extracted File has " + data_lines.length + " lines");
-
-
 }
 
 
 
 
-// first we require the db_access module that we have created. By 
-// doing this we are exposing the functions to be able to
-// use in this script
-var db_access = require('./db_access');
 
 
 // we now query into the database using one of the functions
