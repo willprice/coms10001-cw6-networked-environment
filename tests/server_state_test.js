2f8851cde697be6de00f09af25e8166b095e36cf
Will Price <willprice94+dev@gmail.com>
2014-04-16 21:04:35 +0100

    Refactor server_state_test for clarity
    * Rename the test cases
    * Add comment defining structure of test cases

--- tests/server_state_test.js
+++ tests/server_state_test.js
@@ -3,8 +3,9 @@ var ServerState = require('../src/server_state');
 
 var server_state = new ServerState;
 
-var tests = {};
-var emit_tests = {};
+var tests = {
+    events: {}
+};
 
 
 function assert_request_emits_event(test, request, event, state) {
@@ -19,7 +20,8 @@ function assert_request_emits_event(test, request, event, state) {
     server_state.processRequest(null, request);
 }
 
-var emit_test_cases = [
+var event_test_cases = [
+    // [event, request, server_state]
     ["join", "join", server_state.INITIALISED],
     ["move", "move,1,1,Taxi", server_state.RUNNING],
     ["next_player", "next_player", server_state.RUNNING],
@@ -30,15 +32,14 @@ var emit_test_cases = [
     ["get_file", "get_file,map", server_state.INITIALISED]
 ];
 
-emit_test_cases.forEach(function(test_case) {
+event_test_cases.forEach(function(test_case) {
     var event = test_case[0];
     var request =  test_case[1];
     var state = test_case[2];
 
-    emit_tests["test_" + event] = function(test) {
+    tests.events["test_" + event] = function(test) {
         assert_request_emits_event(test, request, event, state);
     };
 });
 
-tests.emit_tests = emit_tests;
 module.exports = tests;
